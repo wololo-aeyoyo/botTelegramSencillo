fa9e8afe8a45c8aa638855c6f74643998ed7a951
Alvaro Gabriel <alvarogabrielgomez@gmail.com>
2021-04-12 17:09:21 -0300

    arreglando alguna identacion, utilizando promises donde va, y mejorando el script de /dolar

--- modulos/dolar.js
+++ modulos/dolar.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
+const { patrificarDolares } = require('../modulos/intl');
 
-module.exports = async function dollar(cantidad=1){
-  try{
-    if (cantidad != "/dolar") {var cantidad = parseFloat(cantidad.replace("/dolar ",""));}
-    const response = await axios.get("http://s3.amazonaws.com/dolartoday/data.json");
-    console.log("paopsdfasmpfd");
-    return isNaN(cantidad) ? response.data.USD.transferencia : response.data.USD.transferencia  * cantidad; // operador ternario para saber si mandaron numero o no
-    
-  }
-  catch(error){
-    return error;
-  }
+module.exports = function dollar(comando) {
+  return new Promise(async (resolve, reject) => {
+    console.log("/dolar");
+    try {
+      let response = '', cantidad = 1;
+      if (comando != "/dolar") { 
+        cantidad = parseFloat(comando.replace("/dolar ","")); 
+        response = `${cantidad} dolares son: `
+      }
+      const apiResponse = await axios.get("http://s3.amazonaws.com/dolartoday/data.json");
+      if (apiResponse.status === 200) {
+        response += patrificarDolares(apiResponse.data.USD.transferencia  * cantidad);
+      }
+      resolve(response); 
+    }
+    catch(error){
+      reject(error);
+    }
+  });
 }
