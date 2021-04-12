98b205196d80b85ec40f907cccb8d038096cd8a0
humberto raniolo <humberto.raniolo@gmail.com>
2021-04-12 19:45:29 -0400

    añadido el regex y mas memes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Telegraf } = require("telegraf")
+const { Telegraf, Context } = require("telegraf")
 const getDollar = require("./modulos/dolar");
 const getBooru = require("./modulos/gelburu");
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -14,25 +14,38 @@ bot.start((ctx)=>{
 })
 
 // Kuek
-bot.hears(["gabriel","Gabriel","GABRIEL"], ctx =>{
+bot.hears(/(gabriel)/gmi, ctx =>{
     ctx.replyWithPhoto(
-        {url:"https://i2-prod.mirror.co.uk/incoming/article1448101.ece/ALTERNATES/s615b/Skeleton%20in%20Sweden"},
-        {caption: "El pana @GabrielReinoza esta zeko zeko rip"}
+        {url:"https://i2-prod.mirror.co.uk/incoming/article1448101.ece/ALTERNATES/s615b/Skeleton%20in%20Sweden"});
+
+    ctx.reply("lo quiero mucho al pana @Miguel129");
+})
+
+bot.hears(/(marico el que lo lea)/gmi, ctx => {
+    ctx.reply(`mas marico es el pana @${ctx.from.username}`);
+});
+
+bot.hears(/(miguel)/gmi, ctx => {
+    ctx.replyWithVideo({ source: "./videos/el pana Miguel.mp4"});
+    
+});
+
+
+
+bot.hears("megumi", ctx => {
+    ctx.replyWithVideo(
+        { source: "./videos/megumi.mp4"},
+        {caption: "El pana @GabrielReinoza esta zeko zeko rip"});
+});
+
+bot.hears(/(kelly)/gmi, ctx =>{
+    ctx.replyWithPhoto(
+        {url:"https://img3.gelbooru.com//samples/ca/4b/sample_ca4bd61b35c07de154b7ab216779df2a.jpg"},
+        {caption: "flatchest btfo HOHOHOOHOHOHO"}
        
        )
 })
 
-bot.hears("marico el que lo lea", ctx => {
-    ctx.reply(`mas marico es el pana @${ctx.from.username}`);
-});
-
-bot.hears("gabriel", ctx => {
-    ctx.reply("ese pana esta como mongasama pero zeko zeko");
-});
-
-bot.hears("megumi", ctx => {
-    ctx.replyWithVideo({ source: "./videos/megumi.mp4"});
-});
 
 bot.command("/dolar", ctx => {
     getDollar(ctx.message.text)
