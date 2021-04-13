e79b33528871c87f0c938cad2b444940b6e17e1e
humberto raniolo <humberto.raniolo@gmail.com>
2021-04-12 22:17:36 -0400

    esponja

--- main.js
+++ main.js
@@ -16,7 +16,11 @@ bot.start((ctx)=>{
 // Kuek
 bot.hears(/(gabriel)/gmi, ctx =>{
     ctx.replyWithPhoto(
-        {url:"https://i2-prod.mirror.co.uk/incoming/article1448101.ece/ALTERNATES/s615b/Skeleton%20in%20Sweden"});
+        {url:"https://i2-prod.mirror.co.uk/incoming/article1448101.ece/ALTERNATES/s615b/Skeleton%20in%20Sweden"},
+        {caption: "El pana @GabrielReinoza esta zeko zeko rip"}
+
+        
+        );
 
 })
 
@@ -35,13 +39,21 @@ bot.hears(/(miguel)/gmi, ctx => {
 bot.hears("megumi", ctx => {
     ctx.replyWithVideo(
         { source: "./videos/megumi.mp4"},
-        {caption: "El pana @GabrielReinoza esta zeko zeko rip"});
+        {caption: "megumiiiiii"});
 });
 
 bot.hears(/(kelly)/gmi, ctx =>{
     ctx.replyWithPhoto(
         {url:"https://img3.gelbooru.com//samples/ca/4b/sample_ca4bd61b35c07de154b7ab216779df2a.jpg"},
         {caption: "flatchest btfo HOHOHOOHOHOHO"}
+       
+       )
+})
+
+bot.hears(/(frankcys)/gmi, ctx =>{
+    ctx.replyWithPhoto(
+        {url:"https://i.imgur.com/a82u1j9.jpg"},
+        {caption: "Esponja para dejarlos zeko zeko"}
        
        )
 })
